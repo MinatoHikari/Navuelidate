32c946839911c64278ef01b93cadefc4d3c63836
MinatoHikari <35342316+MinatoHikari@users.noreply.github.com>
2023-02-13 14:23:28 +0800

    fix export

--- package/index.ts
+++ package/index.ts
@@ -1,5 +1,5 @@
 export * from './utils';
 export * from './types';
-export { useFormCreator } from './composable/useFormCreator';
+export { useFormCreator, createDefaultSettings } from './composable/useFormCreator';
 import Component from './comonent/FormCreator.vue';
 export const FormCreator = Component;
